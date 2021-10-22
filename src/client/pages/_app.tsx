380261609044a620ae86b245af3b55c34690a35c
Tahmid Hasan <web.tahmid@outlook.com>
2021-10-22 22:43:32 +0600

    Update next structure

--- src/client/pages/_app.tsx
+++ src/client/pages/_app.tsx
@@ -1,6 +1,6 @@
 import type { AppProps } from 'next/app'
 
-import '../styles.css'
+import '../styles/styles.css'
 
 function App({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />
